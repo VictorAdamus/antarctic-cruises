181f20401df937b468faa83f2a13a363e54073a4
Victor Adamus <ak47unit@gmail.com>
2023-01-11 00:11:47 +0300

    исправил изображения на промо-блок

--- source/js/utils/scroll-to-block.js
+++ source/js/utils/scroll-to-block.js
@@ -1,60 +1,60 @@
 const scrollToBlock = () => {
-    const scrollToAdvantages = () => {
-        const scrollButton = document.querySelector('[data-scroll-advantages]');
-        const block = document.querySelector('[data-advantages]');
-        scrollButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            block.scrollIntoView({
-                block: 'center',
-                behavior: 'smooth',
-            })
-            console.log('adv');
-        })
-    }
+  const scrollToAdvantages = () => {
+    const scrollButton = document.querySelector('[data-scroll-advantages]');
+    const block = document.querySelector('[data-advantages]');
+    scrollButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      block.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth',
+      })
+      console.log('adv');
+    })
+  }
 
-    const scrollToBooking = () => {
-        const scrollButton = document.querySelector('[data-scroll-booking]');
-        const block = document.querySelector('[data-booking]');
-        scrollButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            block.scrollIntoView({
-                block: 'center',
-                behavior: 'smooth',
-            })
-            console.log('book');
-        })
-    }
+  const scrollToBooking = () => {
+    const scrollButton = document.querySelector('[data-scroll-booking]');
+    const block = document.querySelector('[data-booking]');
+    scrollButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      block.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth',
+      })
+      console.log('book');
+    })
+  }
 
-    const scrollToProducts = () => {
-        const scrollButton = document.querySelector('[data-scroll-products]');
-        const block = document.querySelector('[data-products]');
-        scrollButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            block.scrollIntoView({
-                block: 'center',
-                behavior: 'smooth',
-            })
-            console.log('prod');
-        })
-    }
+  const scrollToProducts = () => {
+    const scrollButton = document.querySelector('[data-scroll-products]');
+    const block = document.querySelector('[data-products]');
+    scrollButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      block.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth',
+      })
+      console.log('prod');
+    })
+  }
 
-    const scrollToLocation = () => {
-        const scrollButton = document.querySelector('[data-scroll-location]');
-        const block = document.querySelector('[data-location]');
-        scrollButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            block.scrollIntoView({
-                block: 'center',
-                behavior: 'smooth',
-            })
-            console.log('loc');
-        })
-    }
+  const scrollToLocation = () => {
+    const scrollButton = document.querySelector('[data-scroll-location]');
+    const block = document.querySelector('[data-location]');
+    scrollButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      block.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth',
+      })
+      console.log('loc');
+    })
+  }
 
-    scrollToAdvantages();
-    scrollToBooking();
-    scrollToProducts();
-    scrollToLocation();
+  scrollToAdvantages();
+  scrollToBooking();
+  scrollToProducts();
+  scrollToLocation();
 }
 
 export {scrollToBlock};
