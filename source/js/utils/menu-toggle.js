8258c4ce789c839d313c2efb9c99789032a77c65
Victor Adamus <ak47unit@gmail.com>
2023-01-13 13:23:18 +0300

    правки после дебага-1

--- source/js/utils/menu-toggle.js
+++ source/js/utils/menu-toggle.js
@@ -4,6 +4,9 @@ const menuToggle = () => {
   const navMenu = document.querySelector('[data-header-nav]');
   const logo = document.querySelector('[data-logo-link]');
   const navLinks = document.querySelectorAll('[data-link]');
+  const overlay = document.querySelector('[data-overlay]');
+  const body = document.body;
+
   header.classList.remove('header--no-js');
   toggle.classList.remove('toggle--no-js');
   const openMenu = function () {
@@ -11,9 +14,13 @@ const menuToggle = () => {
     navMenu.classList.add('nav-menu--show');
     logo.classList.add('header__logo-link--custom');
     header.classList.add('header--menu-open');
+    overlay.classList.add('overlay--menu-open');
+    window.scroll(0, 0);
+    body.classList.add('body--menu-open');
     navLinks.forEach((link) => {
       link.addEventListener('click', linkHandler);
     });
+    overlay.addEventListener('click', overlayHandler);
   };
 
   const closeMenu = function () {
@@ -21,12 +28,19 @@ const menuToggle = () => {
     navMenu.classList.remove('nav-menu--show');
     logo.classList.remove('header__logo-link--custom');
     header.classList.remove('header--menu-open');
+    body.classList.remove('body--menu-open');
+    overlay.classList.remove('overlay--menu-open');
     navLinks.forEach((link) => {
       link.removeEventListener('click', linkHandler);
     });
+    overlay.removeEventListener('click', overlayHandler);
   };
 
   const linkHandler = () => {
+    closeMenu();
+  };
+
+  const overlayHandler = () => {
     closeMenu();
   };
 
